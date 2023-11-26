ad1932263f7f62a56fc67acfcd464b710e267f0d
Jennny1 <june9282@gmail.com>
2023-11-26 15:52:48 +0900

    "게시글 작성"

---
+++ src/main/java/com/example/yummytable/YummyTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.yummytable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yummytable/YummyTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yummytable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
