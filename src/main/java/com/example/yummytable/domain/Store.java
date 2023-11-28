f4596d59413a9e206a9473d52e49603255f0e749
Jennny1 <june9282@gmail.com>
2023-11-28 13:25:41 +0900

    "예약기능 구현"

---
+++ src/main/java/com/example/yummytable/domain/Store.java
@@ -0,0 +1,64 @@
+package com.example.yummytable.domain;
+
+import com.example.yummytable.type.BoardStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "board")
+public class Store {
+
+  // 게시글 관련 정보
+  @Id
+  @GeneratedValue
+  private Long boardId;
+  private String title;
+  private String content;
+  private String password;
+  @Enumerated(EnumType.STRING)
+  private BoardStatus boardStatus;
+
+  @CreatedDate
+  private LocalDateTime registeredAt;
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+  private LocalDateTime unregisteredAt;
+
+  // 식당 관련 정보
+  private String storeName;
+  private String keyword;
+  private Double locationX;
+  private Double locationY;
+  private String menu;
+  private int capacity;
+
+
+  // 예약정보
+  @OneToMany
+  @JoinColumn(name = "booking")
+  private List<Booking> booking;
+
+}
