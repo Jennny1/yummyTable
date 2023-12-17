7967bfa84519446d9afa6090aabbb5803a9cf69d
Jennny1 <june9282@gmail.com>
2023-12-17 21:23:47 +0900

    refactor(댓글 기능): 댓글 기능 수정
    로그인 여부 확인 후 동작

--- src/main/java/com/example/yummytable/service/CommentService.java
+++ src/main/java/com/example/yummytable/service/CommentService.java
@@ -10,6 +10,7 @@ import com.example.yummytable.dto.comment.UpdateComment.Request;
 import com.example.yummytable.exception.yummyException;
 import com.example.yummytable.repository.BoardRepository;
 import com.example.yummytable.repository.CommentRepository;
+import com.example.yummytable.repository.MemberRepository;
 import com.example.yummytable.type.ErrorCode;
 import com.example.yummytable.type.Status;
 import jakarta.transaction.Transactional;
@@ -28,12 +29,22 @@ public class CommentService {
 
   private final CommentRepository commentRepository;
   private final BoardRepository boardRepository;
+  private final MemberRepository memberRepository;
 
-  /*댓글 생성
-   * - 게시글 생성 선행 필수
-   * */
+  /**
+   * 댓글 생성 게시글 생성 선행 필수
+   *
+   * @param boardId
+   * @param memberId
+   * @param request
+   * @return
+   */
   public CommentDto createComment(Long boardId, Long memberId,
       @Valid CreateComment.Request request) {
+
+    // 로그인 여부 확인
+    validLogin(memberId);
+
     // 게시글 존재 확인
     Optional<Board> board = Optional.ofNullable(boardRepository.findByBoardId(boardId)
         .orElseThrow(() -> new yummyException(ErrorCode.BOARD_NOT_FOUND)));
@@ -50,11 +61,21 @@ public class CommentService {
     ));
   }
 
+  /**
+   * 댓글 삭제
+   *
+   * @param commentId
+   * @param boardId
+   * @param memberId
+   * @param request
+   * @return
+   */
+  public CommentDto deleteComment(Long commentId, Long boardId, Long memberId,
+      @Valid Request request) {
+    // 로그인 여부 확인
+    validLogin(memberId);
 
-  /*댓글 삭제*/
-  public CommentDto deleteComment(Long commentId, Long boardId, @Valid Request request) {
     Optional<Comment> comment = validCommentInfo(commentId, boardId, request);
-
     comment.get().setCommentStatus(Status.DELETE);
     comment.get().setUnregisteredAt(LocalDateTime.now());
     commentRepository.save(comment.get());
@@ -64,12 +85,22 @@ public class CommentService {
 
   }
 
+  /**
+   * 댓글 수정
+   *
+   * @param boardId
+   * @param commentId
+   * @param memberId
+   * @param request
+   * @return
+   */
+  public CommentDto updateComment(Long boardId, Long commentId, Long memberId,
+      @Valid UpdateComment.Request request) {
 
+    // 로그인 여부 확인
+    validLogin(memberId);
 
-  /*댓글 수정*/
-  public CommentDto updateComment(Long boardId, Long commentId, @Valid UpdateComment.Request request) {
     Optional<Comment> comment = validCommentInfo(commentId, boardId, request);
-
     if (request.getContent().equals("")) {
       throw new yummyException(ErrorCode.CONTENT_IS_NULL);
     }
@@ -81,22 +112,31 @@ public class CommentService {
     return CommentDto.formEntity(comment.get());
   }
 
-  /*댓글 보기 - 최신순*/
-  public List<CommentDto> getCommentBylatest(Long boardId) {
+
+  /**
+   * 댓글 보기 최신순
+   * @param boardId
+   * @param memberId
+   * @return
+   */
+  public List<CommentDto> getCommentBylatest(Long boardId, Long memberId) {
+    // 로그인 여부 확인
+    validLogin(memberId);
 
     // 게시글 존재 확인
     Board board = boardRepository.findByBoardId(boardId)
         .orElseThrow(() -> new yummyException(ErrorCode.BOARD_NOT_FOUND));
 
     // 댓글 리스트 가져오기
-    List<Comment> comments = commentRepository.findByBoardBoardIdOrderByBoardRegisteredAtAsc(boardId);
+    List<Comment> comments = commentRepository.findByBoardBoardIdOrderByBoardRegisteredAtAsc(
+        boardId);
 
     return comments.stream().map(CommentDto::formEntity).collect(Collectors.toList());
   }
 
 
-
-  private Optional<Comment> validCommentInfo(Long commentId, Long boardId, UpdateComment.@Valid Request request) {
+  private Optional<Comment> validCommentInfo(Long commentId, Long boardId,
+      UpdateComment.@Valid Request request) {
     // 게시글 존재 확인
     Optional<Board> board = Optional.ofNullable(boardRepository.findByBoardId(boardId)
         .orElseThrow(() -> new yummyException(ErrorCode.BOARD_NOT_FOUND)));
@@ -111,5 +151,15 @@ public class CommentService {
       throw new yummyException(ErrorCode.PASSWORD_NOT_MATCH);
     }
     return comment;
+  }
+
+  private void validLogin(Long memberId) {
+    // 로그인 여부 확인
+    Member byMemberId = memberRepository.findByMemberIdAndMemberStatus(memberId, Status.EXISTENT)
+        .orElseThrow(() -> new yummyException(ErrorCode.MEMBER_IS_NOT_EXIST));
+
+    if (byMemberId.getToken() == null || byMemberId.getToken().isEmpty()) {
+      throw new yummyException(ErrorCode.NOT_LOGGED_IN);
+    }
   }
 }
